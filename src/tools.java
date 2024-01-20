3f29b45273bc862ae5e681b947f1aff8b5d6d4f7
IDlike <2808933646@qq.com>
2024-01-20 18:46:53 +0800

    ...

---
+++ src/tools.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+
+public class tools {
+    public static boolean TextClearNullLine(String file){
+
+        if(!new File(file).isFile()){
+            System.out.println("E: 打不开文件");
+            return false;
+        }
+        List<String> a = TeipMake.ReadCFGFile(file);
+        StringBuilder buffer_text = new StringBuilder();
+        for (int i = 0; i < a.size(); i++) {
+            if(!Objects.equals(a.get(i), "")){
+                buffer_text.append(a.get(i)).append("\r\n");
+            }
+        }
+        TeipMake.WriteFileToThis(file, String.valueOf(buffer_text));
+        return true;
+    }
+}
